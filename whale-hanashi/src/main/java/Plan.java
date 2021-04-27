28a02a45e5b70caf5be3c1d93387a17c4944418c
SuCicada <pengyifu@gmail.com>
2021-04-27 20:33:56 +0800

    merge me plz (#2)
    * init: 話し module
    
    * modify:
    实现了
    - driver发送函数体给server, 通过socket
    - 函数体被 Server 反序列化后成功执行
    
    * modify:
    实现了
    - 封装执行计划 plan (目前是特定功能)
    - server将执行完成的结果返回给driver
    - 参考flink方式, 在server中加入 listener
    
    Co-authored-by: pengyifu <yifu.peng@mail.hypers.com>

---
+++ whale-hanashi/src/main/java/Plan.java
@@ -0,0 +1,24 @@
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.function.Function;
+
+@Builder
+@Getter
+public class Plan implements Serializable {
+    public interface MyFunc<T, R> extends Serializable, Function<T, R> {
+    }
+
+    int start, end;
+    public MyFunc func;
+
+    static class PlanBuilder {
+        public PlanBuilder setRange(int start, int end) {
+            this.start = start;
+            this.end = end;
+            return this;
+        }
+    }
+}
---
+++ whale-hanashi/src/main/java/VoidSupplier.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface VoidSupplier {
+    void apply() throws Exception;
+}
